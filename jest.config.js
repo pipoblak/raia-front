edbb5a5fd8756b80034715a471aeac034a1b1a98
Felipe <pipoblakgamer@gmail.com>
2020-12-04 14:21:31 -0300

    Adding and Setuping Jest and TestingLibrary

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/stylesMock.js"
+  },
+  bail: 1,
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['components/**/*.js', 'pages/**/*.js'],
+  coverageReporters: ['lcov', 'text'],
+};
